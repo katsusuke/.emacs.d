bbd0a33aa463a918c63bc51c3e7e14d9476b053f
Katsusuke Shimizu <katsusuke@gmail.com>
2012-04-12 11:19:35 +0900

    バッファを入れ替える swap-screen コマンドを追加

--- .emacs.el
+++ .emacs.el
@@ -202,6 +202,28 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
+;;汎用機の SPF (mule みたいなやつ) には
+;;画面を 2 分割したときの 上下を入れ替える swap screen
+;;というのが PF 何番かにわりあてられていました。
+(defun swap-screen()
+  "Swap two screen,leaving cursor at current window."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (nextbuf (window-buffer (next-window))))
+    (set-window-buffer (next-window) (window-buffer))
+    (set-window-buffer thiswin nextbuf)))
+(defun swap-screen-with-cursor()
+  "Swap two screen,with cursor in same buffer."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (thisbuf (window-buffer)))
+    (other-window 1)
+    (set-window-buffer thiswin (window-buffer))
+    (set-window-buffer (selected-window) thisbuf)))
+;(global-set-key [f2] 'swap-screen)
+;(global-set-key [S-f2] 'swap-screen-with-cursor)
+
+
 ;; Don't show Welcome Page
 (setq inhibit-startup-message t)
 ;; Stop beep and flush
