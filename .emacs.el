cb5344ea73eb651c67d04df633056c75b4132adc
Katsusuke Shimizu <katsusuke@gmail.com>
2014-12-12 17:54:59 +0900

    remove el-get

--- .emacs.el
+++ .emacs.el
@@ -197,7 +197,6 @@
     (add-to-list 'load-path path)))
 
 (add-load-path "~/.emacs.d/lisp")
-(add-load-path "~/.emacs.d/el-get")
 
 (if (eq window-system 'w32)
     (if (file-accessible-directory-p "c:/cygwin")
@@ -212,31 +211,35 @@
       (setq shell-file-name "C:/cygwin64/bin/bash.exe")
       (setq explicit-shell-file-name "C:/cygwin64/bin/bash.exe")))
 
-
-;; el-get パッケージマネージャ
-(require 'el-get)
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(el-get-dir "~/.emacs.d/el-get-packages/")
  '(gud-gdb-command-name "gdb --annotate=1")
  '(large-file-warning-threshold nil)
- '(safe-local-variable-values (quote ((ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t))))
+ '(safe-local-variable-values
+   (quote ((ruby-compilation-executable . "ruby") 
+	   (ruby-compilation-executable . "ruby1.8")
+	   (ruby-compilation-executable . "ruby1.9")
+	   (ruby-compilation-executable . "rbx")
+	   (ruby-compilation-executable . "jruby")
+	   (eval
+	    ignore-errors
+	    "Write-contents-functions is a buffer-local alternative to before-save-hook"
+	    (add-hook
+	     (quote write-contents-functions)
+	     (lambda nil (delete-trailing-whitespace) nil))
+	    (require (quote whitespace))
+	    "Sometimes the mode needs to be toggled off and on."
+	    (whitespace-mode 0)
+	    (whitespace-mode 1))
+	   (whitespace-line-column . 80)
+	   (whitespace-style face trailing lines-tail)
+	   (require-final-newline . t))))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
 
 ;; リージョンをハイライト
 ;; C-g で解除(マークは残っているがリージョンは無効)
