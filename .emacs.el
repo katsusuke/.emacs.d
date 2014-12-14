97c231000b21094d2e224be26f923d67bdf3bf57
Katsusuke Shimizu <katsusuke@gmail.com>
2014-12-14 15:07:53 +0900

    Auto revert をやめる

--- .emacs.el
+++ .emacs.el
@@ -282,13 +282,38 @@
 
 ;; 自動でrevert-buffer
 ;; ↓こいつをnon-nilにしておくと、vcsによる変更もチェックしてくれる
-(setq auto-revert-check-vc-info t)
-(setq auto-revert-interval 30)
-(add-hook 'find-file-hook
-	  '(lambda ()
-	     (when (and buffer-file-name
-			(vc-backend buffer-file-name))
-	       (auto-revert-mode))))
+;; (setq auto-revert-check-vc-info t)
+;; (setq auto-revert-interval 30)
+;; (add-hook 'find-file-hook
+;; 	  '(lambda ()
+;; 	     (when (and buffer-file-name
+;; 			(vc-backend buffer-file-name))
+;; 	       (auto-revert-mode))))
+
+ 	
+
+;; http://namazu.org/~satoru/diary/?200203c&to=200203272#200203272
+;; 編集中のファイルを開き直す
+;; - yes/no の確認が不要;;   - revert-buffer は yes/no の確認がうるさい
+;; - 「しまった! 」というときにアンドゥで元のバッファの状態に戻れる
+;;   - find-alternate-file は開き直したら元のバッファの状態に戻れない
+;;
+(defun reopen-file ()
+  (interactive)
+  (let ((file-name (buffer-file-name))
+        (old-supersession-threat
+         (symbol-function 'ask-user-about-supersession-threat))
+        (point (point)))
+    (when file-name
+      (fset 'ask-user-about-supersession-threat (lambda (fn)))
+      (unwind-protect
+          (progn
+            (erase-buffer)
+            (insert-file file-name)
+            (set-visited-file-modtime)
+            (goto-char point))
+        (fset 'ask-user-about-supersession-threat
+              old-supersession-threat)))))
 
 ;; リージョンの行数を表示
 (defun count-lines-and-chars ()
