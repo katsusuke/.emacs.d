5bb1a23725b8419559c39cdba92323c373b9b54c
Katsusuke Shimizu <katsusuke@gmail.com>
2015-01-06 14:55:37 +0900

    helm-migemo 追加

--- .emacs.el
+++ .emacs.el
@@ -36,6 +36,7 @@
     helm-rails
     helm-ag
     helm-ls-git
+    helm-migemo
     migemo
     rvm
 ;    yasnippet
@@ -468,6 +469,22 @@
 
 ;; projectile
 (helm-projectile-on)
+
+;; helm-migemo
+(require 'helm-migemo)
+;;; http://rubikitch.com/2014/12/19/helm-migemo/
+(eval-after-load "helm-migemo"
+  '(defun helm-compile-source--candidates-in-buffer (source)
+     (helm-aif (assoc 'candidates-in-buffer source)
+         (append source
+                 `((candidates
+                    . ,(or (cdr it)
+                           (lambda ()
+                             ;; Do not use `source' because other plugins
+                             ;; (such as helm-migemo) may change it
+                             (helm-candidates-in-buffer (helm-get-current-source)))))
+                   (volatile) (match identity)))
+       source)))
 
 ;; auto-complete
 (require 'auto-complete-config)
