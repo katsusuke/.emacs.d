f9d59710e3db7475c9f88a33cce3470be4582b4e
Katsusuke Shimizu <katsusuke@gmail.com>
2015-01-06 15:00:17 +0900

    helm-swoop 追加

--- .emacs.el
+++ .emacs.el
@@ -37,6 +37,8 @@
     helm-ag
     helm-ls-git
     helm-migemo
+    helm-swoop
+    ace-isearch
     migemo
     rvm
 ;    yasnippet
@@ -485,6 +487,37 @@
                              (helm-candidates-in-buffer (helm-get-current-source)))))
                    (volatile) (match identity)))
        source)))
+
+;;; http://rubikitch.com/2014/12/25/helm-swoop/
+(require 'helm-swoop)
+;;; isearchからの連携を考えるとC-r/C-sにも割り当て推奨
+(define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+(define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+
+;;; 検索結果をcycleしない、お好みで
+(setq helm-swoop-move-to-line-cycle nil)
+
+(cl-defun helm-swoop-nomigemo (&key $query ($multiline current-prefix-arg))
+  "シンボル検索用Migemo無効版helm-swoop"
+  (interactive)
+  (let ((helm-swoop-pre-input-function
+         (lambda () (format "\\_<%s\\_> " (thing-at-point 'symbol)))))
+    (helm-swoop :$source (delete '(migemo) (copy-sequence (helm-c-source-swoop)))
+                :$query $query :$multiline $multiline)))
+;;; C-M-:に割り当て
+(global-set-key (kbd "C-M-:") 'helm-swoop-nomigemo)
+
+;;; [2014-11-25 Tue]
+(when (featurep 'helm-anything)
+  (defadvice helm-resume (around helm-swoop-resume activate)
+    "helm-anything-resumeで復元できないのでその場合に限定して無効化"
+    ad-do-it))
+
+;;; ace-isearch
+(global-ace-isearch-mode 1)
+
+;;; ここまで
+
 
 ;; auto-complete
 (require 'auto-complete-config)
