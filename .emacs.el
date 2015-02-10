937f275648bc8260c602b41865e4b0344f719e34
Katsusuke Shimizu <katsusuke@gmail.com>
2015-02-10 12:59:14 +0900

    coffee-mode のインデントを2スペースに変更

--- .emacs.el
+++ .emacs.el
@@ -563,7 +563,8 @@
 (defun coffee-custom ()
   "coffee-mode-hook"
   (setq indent-tabs-mode nil)
-  (set (make-local-variable 'tab-width) 2))
+  (set (make-local-variable 'tab-width) 8)
+  (setq coffee-tab-width 2))
 (add-hook 'coffee-mode-hook
   '(lambda()
      (coffee-custom)))
