7e087677a53512a33989783dab59782e61032433
Katsusuke Shimizu <katsusuke@gmail.com>
2015-05-02 15:18:19 +0900

    フォントサイズ変更可能に

--- .emacs.el
+++ .emacs.el
@@ -140,28 +140,13 @@
 		       '(height . 50)    ; フレームの高さ
 		       '(alpha . 85)
 		       )default-frame-alist))
-	;; フォント設定 :height を変えるとサイズが変わる
-	;; 他をいじるとカオス
-	(set-face-attribute 'default nil :family "monaco" :height 100)
-	(set-fontset-font
-	 (frame-parameter nil 'font)
-	 'japanese-jisx0208
-	 '("Hiragino Kaku Gothic ProN" . "iso10646-1"))
-	(set-fontset-font
-	 (frame-parameter nil 'font)
-	 'japanese-jisx0212
-	 '("Hiragino Kaku Gothic ProN" . "iso10646-1")) 
-	(set-fontset-font
-	 (frame-parameter nil 'font)
-	 'mule-unicode-0100-24ff
-	 '("monaco" . "iso10646-1"))
-	(setq face-font-rescale-alist
-	      '(("^-apple-hiragino.*" . 1.2)
-		(".*osaka-bold.*" . 1.2)
-		(".*osaka-medium.*" . 1.2)
-		(".*courier-bold-.*-mac-roman" . 1.0)
-		(".*monaco cy-bold-.*-mac-cyrillic" . 0.9) (".*monaco-bold-.*-mac-roman" . 0.9)
-		("-cdac$" . 1.3)))
+
+	(set-face-attribute 'default nil :family "Menlo" :height 140)
+	(set-fontset-font (frame-parameter nil 'font)
+			  'japanese-jisx0208
+			  (font-spec :family "Hiragino Kaku Gothic ProN"))
+	(add-to-list 'face-font-rescale-alist
+		     '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
 	)
       (set-frame-default)
       ;; コマンドから open -a Emacs.app されたときに新しいフレームを開かない
