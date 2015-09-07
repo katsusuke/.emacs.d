f6caef5b8a1c6edac9abd3f57bca4846f4a38f9d
Katsusuke Shimizu <katsusuke@gmail.com>
2015-09-07 12:56:51 +0900

    Fix helm-mimgemo

--- .emacs.el
+++ .emacs.el
@@ -459,18 +459,21 @@
 ;; helm-migemo
 (require 'helm-migemo)
 ;;; http://rubikitch.com/2014/12/19/helm-migemo/
-(eval-after-load "helm-migemo"
-  '(defun helm-compile-source--candidates-in-buffer (source)
-     (helm-aif (assoc 'candidates-in-buffer source)
-         (append source
-                 `((candidates
-                    . ,(or (cdr it)
-                           (lambda ()
-                             ;; Do not use `source' because other plugins
-                             ;; (such as helm-migemo) may change it
-                             (helm-candidates-in-buffer (helm-get-current-source)))))
-                   (volatile) (match identity)))
-       source)))
+(with-eval-after-load "helm-migemo"
+  (defun helm-compile-source--candidates-in-buffer (source)
+    (helm-aif (assoc 'candidates-in-buffer source)
+        (append source
+                `((candidates
+                   . ,(or (cdr it)
+                          (lambda ()
+                            ;; Do not use `source' because other plugins
+                            ;; (such as helm-migemo) may change it
+                            (helm-candidates-in-buffer (helm-get-current-source)))))
+                  (volatile) (match identity)))
+      source))
+  ;; [2015-09-06 Sun]helm-match-plugin -> helm-multi-match変更の煽りを受けて
+  (defalias 'helm-mp-3-get-patterns 'helm-mm-3-get-patterns)
+  (defalias 'helm-mp-3-search-base 'helm-mm-3-search-base))
 
 ;;; http://rubikitch.com/2014/12/25/helm-swoop/
 (require 'helm-swoop)
