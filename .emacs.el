ea51c18e745e72b74a154c6f6205749ae8e353bb
Katsusuke Shimizu <katsusuke@gmail.com>
2016-01-23 17:47:34 +0900

    remove commentout

--- .emacs.el
+++ .emacs.el
@@ -192,14 +192,6 @@
       (define-key function-key-map [134221148] [?\M-\\])
       (define-key function-key-map [201330012] [?\C-\M-\\])
 
-      ;; スクロールゆっくり
-      ;; (global-set-key [wheel-up] '(lambda () "" (interactive) (scroll-down 1)))
-      ;; (global-set-key [wheel-down] '(lambda () "" (interactive) (scroll-up 1)))
-      ;; (global-set-key [double-wheel-up] '(lambda () "" (interactive) (scroll-down 2)))
-      ;; (global-set-key [double-wheel-down] '(lambda () "" (interactive) (scroll-up 2)))
-      ;; (global-set-key [triple-wheel-up] '(lambda () "" (interactive) (scroll-down 3)))
-      ;; (global-set-key [triple-wheel-down] '(lambda () "" (interactive) (scroll-up 3)))
-
       ;; カーソル行ハイライト
       (defface hlline-face
       	'((((class color)
