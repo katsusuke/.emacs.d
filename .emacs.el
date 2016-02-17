acbd27ed5ff9fe4decbcb1990f47c09603c8d05d
Katsusuke Shimizu <katsusuke@gmail.com>
2016-02-17 16:52:40 +0900

    Add load ~/.custom.el

--- .emacs.el
+++ .emacs.el
@@ -976,3 +976,6 @@ See URL `http://batsov.com/rubocop/'."
 ;; dockerfile-mode
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+(if (file-exists-p "~/.custom.el")
+    (load "~/.custom.el"))
