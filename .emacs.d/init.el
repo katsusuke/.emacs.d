3e7194a6e8ddebea1e8e52a54020a15cedf88816
Katsusuke Shimizu <katsusuke@gmail.com>
2016-12-20 11:44:53 +0900

    kbd helm-swoop

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -378,6 +378,11 @@
   (define-key global-map (kbd "C-;") 'helm-mini)
   (define-key global-map (kbd "M-x")     'helm-M-x)
   (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  ;; helm-swoop
+  (global-set-key (kbd "M-i") 'helm-swoop)
+  (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+  (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+  
   ;; For helm-find-files etc.
   (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
@@ -411,31 +416,7 @@
       helm-source-projectile-projects
       helm-source-recentf
       helm-source-buffer-not-found))
-
-  ;; ;; http://rubikitch.com/2014/12/25/helm-swoop/
-  ;; (require 'helm-swoop)
-  ;; ;; isearchからの連携を考えるとC-r/C-sにも割り当て推奨
-  ;; (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-  ;; (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-
-  ;; ;; 検索結果をcycleしない、お好みで
-  ;; (setq helm-swoop-move-to-line-cycle nil)
-
-  ;; (cl-defun helm-swoop-nomigemo (&key $query ($multiline current-prefix-arg))
-  ;;   "シンボル検索用Migemo無効版helm-swoop"
-  ;;   (interactive)
-  ;;   (let ((helm-swoop-pre-input-function
-  ;; 	   (lambda () (format "\\_<%s\\_> " (thing-at-point 'symbol)))))
-  ;;     (helm-swoop :$source (delete '(migemo) (copy-sequence (helm-c-source-swoop)))
-  ;; 		  :$query $query :$multiline $multiline)))
-  ;; ;; C-M-:に割り当て
-  ;; (global-set-key (kbd "C-M-:") 'helm-swoop-nomigemo)
-  ;; (when (featurep 'helm-anything)
-  ;;   (defadvice helm-resume (around helm-swoop-resume activate)
-  ;;     "helm-anything-resumeで復元できないのでその場合に限定して無効化"
-  ;;     ad-do-it))
-  ;; ;; helm-migemo end
-
+  
   )
 
 (require 'whitespace)
