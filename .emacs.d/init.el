1c7c7f8e048309ce691d84d2cf602c3888c4e79b
Katsusuke Shimizu <katsusuke@gmail.com>
2019-07-02 12:39:40 +0900

    use-package for scss

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -646,9 +646,10 @@
 	     ))
 
 ;; scss-mode
-(autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-(setq scss-compile-at-save nil)
+(use-package scss-mode
+  :mode "\\.scss$"
+  :config
+  (setq scss-compile-at-save nil))
 
 ;; sass-mode
 (autoload 'sass-mode "sass-mode")
