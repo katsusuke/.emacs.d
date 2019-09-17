922b188a3f1e46da794bd066d11030be8d1e8bef
Katsusuke Shimizu <katsusuke@gmail.com>
2019-09-17 15:10:48 +0900

    config fsharp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -782,6 +782,10 @@
   (alchemist-mode)
   (ac-alchemist-setup))
 
+
+(use-package fsharp-mode
+  :mode "\\.fs[iylx]?$")
+
 ;; dash-at-point
 (global-set-key "\C-cd" 'dash-at-point)
 (global-set-key "\C-ce" 'dash-at-point-with-docset)
