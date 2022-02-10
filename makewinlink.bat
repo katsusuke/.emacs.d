e90a5377350b97b47149b86ea140eb61dca9ceb0
Katsusuke Shimizu <katsusuke@gmail.com>
2022-02-10 22:43:57 +0900

    remove outdated file

--- makewinlink.bat
+++
@@ -1,8 +0,0 @@
-del /A:S coffee-mode
-mklink /D .\coffee-mode ..\coffee-mode
-del /A:S haml-mode
-mklink /D .\haml-mode ..\haml-mode
-del /A:S rhtml
-mklink /D .\rhtml ..\rhtml
-del /A:S rinari
-mklink /D .\rinari ..\rinari
